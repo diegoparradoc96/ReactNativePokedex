36113cbde84268bca19ff6a1191bf535a5dbaa3a
Diego Parrado <diegoparrado.cardozo@hotmail.com>
2023-01-17 11:04:26 -0500

    ...

---
+++ src/navigation/FavoriteNavigation.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { createStackNavigator } from "@react-navigation/stack";
+
+import FavoriteScreen from "../screens/Favorite";
+//import { title } from "process";
+
+const Stack = createStackNavigator();
+
+export default function FavoriteNavigation() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Favorite"
+        component={FavoriteScreen}
+        options={{ title: "Favoritos" }}
+      />
+    </Stack.Navigator>
+  );
+}
--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,11 +1,49 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import {} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+/* Api */
+import { getPokemonsApi, getPokemonDetailsByUrlApi } from "../api/pokemon";
+
+/* Components */
+import PokemonList from "../components/PokemonList";
+
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+
+      const pokemonsArray = [];
+      for await (const pokemon of response.results) {
+        const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
+
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0].type.name,
+          order: pokemonDetails.order,
+          image:
+            pokemonDetails.sprites.other["official-artwork"].front_default,
+        });
+      }
+
+      setPokemons([...pokemons, ...pokemonsArray]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView>
-      <Text>Pokedex</Text>
+      <PokemonList pokemons={pokemons} />
     </SafeAreaView>
   );
 }
